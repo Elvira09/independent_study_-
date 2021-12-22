fe9b9b7b27d685bddfa8526fc164a08c486b142a
Elvira09 <zag0909@gmail.com>
2021-12-22 15:39:55 +0300

    Урок 2. Переменные и типы данных

--- Project_02/Program.cs
+++ Project_02/Program.cs
@@ -48,20 +48,4 @@ namespace project {
     }
 }
 
-/*
-            Console.WriteLine("Hello 1"); // выводит текст в консоль С НОВОЙ строки
-            Console.Write("Hello 2"); // выводит текст в консоль В ОДНУ строку
-            Console.Write("Hello 3");
-            Console.Write("Hello 4\n\n"); // '\n' - вариант вывода текста с новой строки при использовании Write
-            Console.Write("Hello 5");
-            Console.WriteLine("Hello 6");
-            Console.ReadKey(); // Метод получение данных от пользователя из терминала ЛИШЬ ОДНОГО СИМВОЛА
-            Console.ReadLine(); // Метод получение данных от пользователя из терминала МНОГО СИМВОЛОВ
-        }
-        */
-
-            num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("First: " + num1 + "  Second: " + num2);
-        }
-    }
-}
+           
