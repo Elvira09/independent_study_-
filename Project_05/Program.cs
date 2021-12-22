4f0dc1309a5bd51e319b4ccc6feb881b1bd02856
Elvira09 <zag0909@gmail.com>
2021-12-22 17:18:32 +0300

    Урок 5. Оператор Switch-case

---
+++ Project_05/Program.cs
@@ -0,0 +1,23 @@
+﻿//Оператор «Switch-case» ( в отличии от if-else проверяет всего одну пременную на возможные варианты)
+using System; 
+
+namespace project { 
+    class Program { 
+        static void Main() { 
+        short user_input = Convert.ToInt16(Console.ReadLine());
+        switch (user_input) { //проверяю переменную 
+            case 5: //если переменная равна значению 5
+                Console.WriteLine("Число 5"); //выполняется следующее условие - в данном случае выводится на консоль сообщение
+                break; //завершение условия
+            case 15: 
+                Console.WriteLine("Число 15"); 
+                break; 
+            default: //включается, если не сработали кейсы case, аналог else
+                Console.WriteLine("Число не подходит под наш вариант"); 
+                break; 
+
+        }
+
+        }
+    }
+}
